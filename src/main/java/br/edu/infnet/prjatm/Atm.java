8e46023a5e9e234134b8f011240b25a82a3e9bd6
Gogs <gogs@fake.local>
2021-02-09 20:13:46 -0200

    commit inicial

---
+++ src/main/java/br/edu/infnet/prjatm/Atm.java
@@ -0,0 +1,37 @@
+package br.edu.infnet.prjatm;
+import java.util.Scanner;
+
+
+
+/**
+ * 
+ * @author veloso
+ *
+ */
+
+public class Atm {
+	
+	public static void main(String[] args) {
+		//8 tipos primitivos boolean, char, float, int, short, double, long, byte
+		//Variaveis recebem valor: literal ou referencia
+		//fsdfsfs
+		
+		/*
+		 sdfsdfsdfsf
+		 sdfsdfsfs 
+		sfsdgfsdfs
+		sfdsdfsfds 
+		 */
+		
+		System.out.println("Bem vindo");
+		System.out.print("Por favor entre com seu numero conta: ");
+		Scanner scan = new Scanner(System.in);
+		int numeroConta = scan.nextInt();
+		System.out.println(" O numero da conta informado foi: "   + numeroConta);
+		
+				
+				
+	}
+	
+
+}
